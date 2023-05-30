947cb54cb74abac5a0fa303285fa3676e03386df
eliarizzetto <elia.rizzetto@gmail.com>
2023-05-30 15:03:05 +0200

    update mapping.py, clean jptr ntbk
    added lines to check if there already are indexes on the relevant field of the db tables.

--- omid_openalex/preprocess/mapping.py
+++ omid_openalex/preprocess/mapping.py
@@ -15,7 +15,6 @@ def create_omid_openalex_mapping(inp_dir:str, db_path:str, out_dir: str) -> None
     os.makedirs(out_dir, exist_ok=True)
     with sql.connect(db_path) as conn:
         cursor = conn.cursor()
-
         for root, dirs, files in os.walk(inp_dir):
             for file_name in tqdm(files):
                 with open(join(root, file_name), 'r', encoding='utf-8') as inp_file, open(join(out_dir, file_name), 'w', encoding='utf-8', newline='') as out_file:
@@ -33,11 +32,10 @@ def create_omid_openalex_mapping(inp_dir:str, db_path:str, out_dir: str) -> None
                             elif curr_id_type == 'pmcid':
                                 curr_lookup_table = 'WorksPmcid'
                             else:
-                                # this function only supports mapping Works IDs (doi, pmid, pmcid) to OMIDs. There
-                                # are no other ID types in the reduced OpenAlex Works tables, so we can safely skip,
-                                # although they have been left in the reduced OC Meta tables.
+                                # this function only supports the mapping of Works IDs (doi, pmid, pmcid) to OMIDs. There
+                                # are no other ID types in the reduced OpenAlex Works tables, so we can safely skip
+                                # processing them, although they have been left in the reduced OC Meta tables.
                                 continue
-                            q_start_time = time.time()
                             query = "SELECT openalex_id FROM {} WHERE supported_id=?".format(curr_lookup_table)
                             cursor.execute(query, (id,))
                             for res in cursor.fetchall():
@@ -46,14 +44,22 @@ def create_omid_openalex_mapping(inp_dir:str, db_path:str, out_dir: str) -> None
                             out_row = {'omid': row['omid'], 'openalex_id': ' '.join(oa_ids), 'type': row['type']}
                             writer.writerow(out_row)
 
-indexes_created = True
 if __name__ == '__main__':
     conn = sql.connect('oa_ids_tables.db')
     cursor = conn.cursor()
-    start_time_idx_doi = time.time()
-    if not indexes_created:
-        cursor.execute("CREATE INDEX idx_doi ON WorksDoi(doi)")
-        cursor.execute("CREATE INDEX idx_pmid ON WorksPmid(pmid)")
-        cursor.execute("CREATE INDEX idx_pmcid ON WorksPmcid(pmcid)")
+    tables_query = "SELECT name FROM sqlite_master WHERE type = 'table';"
+    cursor.execute(tables_query)
+    tables = [t[0] for t in cursor.fetchall()]
+    for tbl in tables:
+        idxs_query = "SELECT name, tbl_name FROM sqlite_master WHERE type = 'index' AND tbl_name = ?;"
+        cursor.execute(idxs_query, (tbl,))
+        res = cursor.fetchall()
+        if not res:
+            print("No indexes found on table {}. Creating indexes for table {} on 'supported_id' field...".format(tbl, tbl))
+            create_idx_query = "CREATE INDEX idx_{}_works ON {}(supported_id);".format(tbl.split('Works')[1].lower(), tbl)
+            cursor.execute(create_idx_query)
+            print("Index created.")
+    start_time = time.time()
     create_omid_openalex_mapping('D:/reduced_meta_tables', 'oa_ids_tables.db', 'D:/omid_openalex_mapping')
+    print("Creating OMID-OpenAlexID map took: {} minutes".format((time.time() - start_time)/60))
     conn.close()
