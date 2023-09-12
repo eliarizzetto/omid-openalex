c3a0fdaae2673c2f9ddd50a2ff47380dd96510ec
eliarizzetto <elia.rizzetto@gmail.com>
2023-09-12 17:02:36 +0200

    update analytics.helper + analytics notebook

--- analytics/helper.py
+++ analytics/helper.py
@@ -13,6 +13,194 @@ import gzip
 import json
 import jsonlines
 import warnings
+import csv
+from typing import Union
+from collections import Counter, defaultdict
+
+def analyse_multi_mapped_omids(file_path:str, res_type='journal article'):
+    """
+    Analyse the multi-mapped OMIDs and returns a tuple of three lists of dictionaries, where each list contains rows where either only OpenAlex Works ID, or only OpenAlex Sources ID, or both Works and Sources ID are present. The lists are filtered by the type of bibliographic resource (e.g. journal article, book, etc.). The function also prints basic statistics about the filtered data.
+    :param file_path: the .csv file storing the table rows containing multi-mapped OMIDs
+    :param res_type: the type of bibliographic resource to filter the data by
+    :return: tupleof thre lists by composition of OAIDs (works, sources, or both)
+    """
+    df = pd.read_csv(file_path, sep=',', header=None, names=['omid', 'openalex_id', 'type'])
+
+
+    filter_type_df = df[df['type'] == res_type]
+    filter_type_df['openalex_id'] = filter_type_df['openalex_id'].str.split(' ')
+
+    all_works = []
+    all_sources = []
+    works_and_sources = []
+
+    for index, row in filter_type_df.iterrows():
+        starts_with_w = all(item.startswith('W') for item in row['openalex_id'])
+        starts_with_s = all(item.startswith('S') for item in row['openalex_id'])
+
+        if starts_with_w and not starts_with_s:
+            all_works.append(row.tolist())
+        elif starts_with_s and not starts_with_w:
+            all_sources.append(row.tolist())
+        else:
+            works_and_sources.append(row.tolist())
+
+    count_works = dict(Counter(len(item[1]) for item in all_works))
+    count_sources = dict(Counter(len(item[1]) for item in all_sources))
+    count_works_and_sources = dict(Counter(len(item[1]) for item in works_and_sources))
+
+    print(f'Total number of OMIDs of {res_type} multi-mapped: {len(all_works) + len(all_sources) + len(works_and_sources)}', end='\n\n')
+
+    print(f'Number of OMIDs of {res_type} multi-mapped to Work IDs only: {len(all_works)}. The following illustrates how these are distributed over the number of OAIDs each OMID is mapped to: {dict(sorted(count_works.items()))}', end='\n\n')
+    print(f'Number of OMIDs of {res_type} multi-mapped to Source IDs only: {len(all_sources)}. The following illustrates how these are distributed over the number of OAIDs each OMID is mapped to: {dict(sorted(count_sources.items()))}', end='\n\n')
+    print(f'Number of OMIDs of {res_type} multi-mapped to both Work and Source IDs: {len(works_and_sources)}. The following illustrates how these are distributed over the number of OAIDs each OMID is mapped to: {dict(sorted(count_works_and_sources.items()))}', end='\n\n')
+
+    return all_works, all_sources, works_and_sources
+
+def add_columns_to_df(df):
+    """
+    Add to the dataframe the columns for the number of OAIDs for each OMID ('oaid_count' column) and the composition of the OAIDs (works, sources, or both) ('composition' column).
+    :param df: the input dataframe, as it is read from the CSV file
+    :return: a dataframe with the two additional columns
+    """
+    # add a column to the dataframe with the number of OAIDs for each OMID
+    df['oaid_count'] = df['openalex_id'].apply(lambda x: len(x.split(' ') if len(x.split(' ')) > 1 else ''))
+
+    # add a column to the dataframe with the composition of the OAIDs (works, sources, or both)
+    df['composition'] = df['openalex_id'].apply(lambda x: 'works' if all(item.startswith('W') for item in x.split(' ')) else 'sources' if all(item.startswith('S') for item in x.split(' ')) else 'both' if any(item.startswith('W') or item.startswith('S') for item in x.split(' ')) else '')
+
+    # remove extra header row
+    df = df[df['composition'] != ''].reset_index(drop=True)
+    return df
+
+def prepare_data_for_filtering(filepath:str)->pd.DataFrame:
+    """
+    Reads the csv file at the specified path and returns a DataFrame with the columns 'omid', 'openalex_id', 'type' and
+    'composition'. NaN values on the 'type' field are replaced with 'Unspecified'.
+    :param filepath: the path to the csv file to be read (which must be of the form: omid, openalex_id, type)
+    :return: a DataFrame with the columns 'omid', 'openalex_id', 'type' and 'composition'
+    """
+    data = pd.read_csv(filepath, sep=',', header=None, names=['omid', 'openalex_id', 'type'])
+    # replace the NaN values in the 'type' column of the primary entities df with 'Unspecified'
+    data['type'].fillna('Unspecified', inplace=True)
+    # add the 'composition' column to the DataFrame
+    data = add_columns_to_df(data)
+    return data
+def filter_multi_mapped_df(df, res_type: Union[str, None], composition: Union[Literal['works', 'sources', 'both'], None], oaid_count: Union[int, None]=2):
+    """
+    Filter the dataframe by the type of resource, the composition of the OAIDs, and the number of OAIDs for a single OMID.
+    :param df: a DF to which columns 'oaid_count' and 'composition' have been added
+    :param res_type:
+    :param composition: only one at a time: 'works', 'sources', or 'both'; set at None if you want to get all the three
+    :param oaid_count: the exact number of OAIDs for a single OMID
+    :return:
+    """
+    if composition not in ['works', 'sources', 'both', None]:
+        raise ValueError('The composition parameter must be one of the following: "works", "sources", "both", or None.')
+    if res_type:
+        if not oaid_count and not composition:
+            filtered_df = df[(df['type'] == res_type)]
+        elif not oaid_count and composition:
+            filtered_df = df[(df['type'] == res_type) &
+                             (df['composition'] == composition)]
+        elif oaid_count and not composition:
+            filtered_df = df[(df['type'] == res_type) &
+                             (df['oaid_count'] == oaid_count)]
+        else:
+            filtered_df = df[(df['type'] == res_type) &
+                             (df['composition'] == composition) &
+                             (df['oaid_count'] == oaid_count)]
+    else:
+        if not oaid_count and not composition:
+            warnings.warn('You need to specify at least one of the two parameters: composition or oaid_count. Otherwise, the whole dataframe is returned.', UserWarning)
+            filtered_df = df
+        elif not oaid_count and composition:
+            filtered_df = df[(df['composition'] == composition)]
+        elif oaid_count and not composition:
+            filtered_df = df[(df['oaid_count'] == oaid_count)]
+        else:
+            filtered_df = df[(df['composition'] == composition) &
+                             (df['oaid_count'] == oaid_count)]
+
+    return filtered_df
+
+def get_ids_uris(df, verbose=True):
+    """
+    Transform the dataframe into a list of dicts with OMIDs and OAIDs written as (clickable) URLs.
+    :param df: any dataframe with columns 'omid' and 'openalex_id'
+    :param verbose: if True, return the list of whole rows; if False, return only the 'omid' and 'openalex_id' columns
+    :return:
+    """
+    result = []
+    oa_url = 'https://api.openalex.org/'
+    oc_url = 'https://opencitations.net/meta/api/v1/metadata/'
+
+
+    for row in df.to_dict(orient='records'):
+        res_row = row
+        res_row['omid'] = oc_url + row['omid'].replace('meta:', 'omid:')
+        res_row['openalex_id'] = list((map(lambda x: oa_url + x, row['openalex_id'].split())))
+
+        if verbose:
+            result.append(res_row)
+        else:
+            result.append({'omid':res_row['omid'], 'openalex_id':res_row['openalex_id']})
+    return result
+
+
+def get_multi_mapped_omids(inp_dir):
+
+    start_time = time.time()
+    res = pd.DataFrame()
+    # Iterate over each CSV file
+    for filename in tqdm(os.listdir(inp_dir)):
+        if filename.endswith('.csv'):
+            filepath = os.path.join(inp_dir, filename)
+            with open(filepath, 'r') as file:
+                reader = csv.DictReader(file, delimiter=',', dialect='unix')
+
+                for row in reader:
+                    oaids = set(row['openalex_id'].split())
+                    if len(oaids) > 1:
+                        row_df = pd.DataFrame([row])
+                        res = pd.concat([res, row_df], ignore_index=True)
+
+    print(f"Getting the multi-mapped OMIDs took {(time.time()-start_time)/60} minutes")
+    return res
+
+
+def get_stats(inp_dir:str):
+    single_mapped_by_type = {}
+    multi_mapped_by_type = {}
+
+    # Iterate over each CSV file
+    for filename in tqdm(os.listdir(inp_dir)):
+        if filename.endswith('.csv'):
+            filepath = os.path.join(inp_dir, filename)
+            with open(filepath, 'r') as file:
+                reader = csv.DictReader(file, delimiter=',', dialect='unix')
+                for row in reader:
+                    oaid_count = len(row['openalex_id'].split())
+                    type = row['type']
+
+                    if oaid_count == 1 and type:
+                        single_mapped_by_type[type] = single_mapped_by_type.get(type, 0) + 1
+                    elif oaid_count == 1 and not type:
+                        single_mapped_by_type['Unspecified'] = single_mapped_by_type.get('Unspecified', 0) + 1
+                    elif oaid_count > 1 and type:
+                        multi_mapped_by_type[type] = multi_mapped_by_type.get(type, 0) + 1
+                    elif oaid_count > 1 and not type:
+                        multi_mapped_by_type['Unspecified'] = multi_mapped_by_type.get('Unspecified', 0) + 1
+    # sort the dicts by decreasing value
+    single_mapped_by_type = dict(sorted(single_mapped_by_type.items(), key=lambda item: item[1], reverse=True))
+    multi_mapped_by_type = dict(sorted(multi_mapped_by_type.items(), key=lambda item: item[1], reverse=True))
+
+
+    print(f"Total number of single-mapped OMIDs: {sum(single_mapped_by_type.values())}. The distribution by type is: {single_mapped_by_type}.\n"
+          f"Total number of multi-mapped OMIDs: {sum(multi_mapped_by_type.values())}. The distribution by type is: {multi_mapped_by_type}.")
+    return single_mapped_by_type, multi_mapped_by_type
+
+
 
 
 def query_reduced_meta_tables(query_list: List[str], inp_dir: str, prefix='doi:'):
@@ -144,6 +332,38 @@ def create_query_lists_oaid(multi_mapped_df: pd.DataFrame) -> tuple:
     works_list = list({s for l in multi_mapped_df.openalex_id for s in l.split() if s.startswith('W')})
     sources_list = list({s for l in multi_mapped_df.openalex_id for s in l.split() if s.startswith('S')})
     return works_list, sources_list
+
+
+def find_inverted_multi_mapped(inp_dir, out_dir, chunk_size=10000):
+    """
+    Find inverted multi-mapped entities, i.e. entities for which multiple OMIDs are mapped to the same OpenAlex ID.
+    :param inp_dir:
+    :param out_dir:
+    :param chunk_size: number of rows to read at a time (for memory efficiency)
+    :return: None
+    """
+    openalex_id_dict = {}
+    csv_files = [os.path.join(inp_dir, filename) for filename in os.listdir(inp_dir) if filename.endswith('.csv')]
+    for file in tqdm(csv_files):
+        chunks = pd.read_csv(file, chunksize=chunk_size)
+        for chunk in chunks:
+            for index, row in chunk.iterrows():
+                openalex_ids = row['openalex_id'].split()
+                omid = row['omid']
+                for openalex_id in openalex_ids:
+                    if openalex_id in openalex_id_dict:
+                        openalex_id_dict[openalex_id].append(omid)
+                    else:
+                        openalex_id_dict[openalex_id] = [omid]
+    print("Finding entities for which multiple OMIDs are mapped to the same OpenAlex ID...")
+    duplicates = {k: v for k, v in openalex_id_dict.items() if len(v) > 1}
+    result_rows = []
+    for openalex_id, omid_list in duplicates.items():
+        for omid in omid_list:
+            result_rows.append({"omid": omid, "openalex_id": openalex_id})
+    result_df = pd.DataFrame(result_rows)
+    print("Number of inverted multi-mapped entities: ", len(result_df))
+    result_df.to_csv(os.path.join(out_dir, "inverted_multi_mapped.csv"), index=False)
 
 # if __name__ == '__main__':
 #     multi_mapped_omids_path = 'multi_mapped_omids.csv'
