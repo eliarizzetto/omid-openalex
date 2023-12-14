77a8c728fa1e193d3136bec0e1c81968a1620341
eliarizzetto <elia.rizzetto@gmail.com>
2023-12-14 02:22:40 +0100

    Update basic_stats.py

--- analytics/basic_stats.py
+++ analytics/basic_stats.py
@@ -1,17 +1,37 @@
-from omid_openalex.mapping import MetaProcessor
+from omid_openalex.mapping import OpenAlexProcessor
 
-mp = MetaProcessor()
 
-count = 0
-omid_only_count = 0
-other_pids_count= 0
-for row in mp.read_compressed_meta_dump('/vltd/data/meta/dump/meta-csv-dump-2023-10/csv_output_current.zip'):
-    count += 1
-    if ' ' in row['id']:
-        other_pids_count += 1
+op = OpenAlexProcessor()
+
+works_dir = '/vltd/data/openalex/dump/data/works'
+sources_dir = '/vltd/data/openalex/dump/data/sources'
+
+w_count = 0
+w_oaid_only_count = 0
+w_other_pids_count= 0
+for row in op.read_compressed_openalex_dump(works_dir):
+    w_count += 1
+    if row['id'].get('doi') or row['id'].get('pmid') or row['id'].get('pmcid'):
+        w_other_pids_count += 1
     else:
-        omid_only_count += 1
+        w_oaid_only_count += 1
 
-print('Number of rows in zipped Meta CSV dump: ', count)
-print('Number of rows with OMIDs only: ', omid_only_count)
-print('Number of rows with other PIDs: ', other_pids_count)
+print('WORKS')
+print('Number of rows in zipped Meta CSV dump: ', w_count)
+print('Number of rows with OAID only: ', w_oaid_only_count)
+print('Number of rows with PIDs supported by OC Meta: ', w_other_pids_count)
+
+s_count = 0
+s_oaid_only_count = 0
+s_other_pids_count= 0
+for row in op.read_compressed_openalex_dump(sources_dir):
+    s_count += 1
+    if row['id'].get('issn') or row['id'].get('wikidata'):
+        s_other_pids_count += 1
+    else:
+        s_oaid_only_count += 1
+
+print('SOURCES')
+print('Number of rows in zipped Meta CSV dump: ', s_count)
+print('Number of rows with OAID only: ', s_oaid_only_count)
+print('Number of rows with PIDs supported by OC Meta: ', s_other_pids_count)
