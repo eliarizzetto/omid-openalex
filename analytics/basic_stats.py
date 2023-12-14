13f1a1bf92dcb515c4f9411ab9bfc9766e669916
eliarizzetto <elia.rizzetto@gmail.com>
2023-12-14 01:24:29 +0100

    Update basic_stats.py

--- analytics/basic_stats.py
+++ analytics/basic_stats.py
@@ -3,7 +3,13 @@ from omid_openalex.mapping import MetaProcessor
 mp = MetaProcessor()
 
 count = 0
+omid_only_count = 0
+other_pids_count= 0
 for row in mp.read_compressed_meta_dump('/vltd/data/meta/dump/meta-csv-dump-2023-10/csv_output_current.zip'):
     count += 1
+    if ' ' in row['id']:
+        other_pids_count += 1
+    else:
+        omid_only_count += 1
 
 print('Number of rows in zipped Meta CSV dump: ', count)
